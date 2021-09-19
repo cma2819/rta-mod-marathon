5113f8868c784b402400d52b4d7bae9bd655b307
Cma <chicchaima@gmail.com>
2021-09-19 07:56:00 +0000

    init workspace

---
+++ src/main/java/net/cmario/rmm/RmmApplication.java
@@ -0,0 +1,16 @@
+package net.cmario.rmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main rmm spring application.
+ */
+@SpringBootApplication
+public class RmmApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RmmApplication.class, args);
+  }
+
+}
---
+++ src/test/java/net/cmario/rmm/RmmApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cmario.rmm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmmApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
