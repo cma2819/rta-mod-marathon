08e68b31252413ef78b2ebddce368215ad38207f
Cma <chicchaima@gmail.com>
2021-09-20 08:56:20 +0000

    repository test use fixture entity

--- src/test/java/net/cmario/rmm/domain/presentation/EventRepositoryTest.java
+++ src/test/java/net/cmario/rmm/domain/presentation/EventRepositoryTest.java
@@ -10,6 +10,7 @@ import net.cmario.rmm.domain.presentation.model.Event;
 import net.cmario.rmm.domain.presentation.model.EventPresentation;
 import net.cmario.rmm.domain.presentation.model.Location;
 import net.cmario.rmm.domain.presentation.repository.EventRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -21,9 +22,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 @AutoConfigureTestDatabase
 class EventRepositoryTest {
+
+  private Event event;
   
   @Autowired
   private EventRepository repository;
+
+  @BeforeEach
+  public void setup() {
+    EventId id = new EventId(UUID.randomUUID());
+    EventPresentation presentation = new EventPresentation("Event Name", "This is RTA event.");
+    this.event = Event.create(id, presentation, null, Boolean.FALSE);
+  }
 
   @Test
   void testProvideNextId() {
@@ -32,23 +42,21 @@ class EventRepositoryTest {
 
   @Test
   void testStoreEventAndFind() {
-    EventId id = new EventId(UUID.randomUUID());
-    EventPresentation presentation = new EventPresentation("Event Name", "This is RTA event.");
-    Event event = Event.create(id, presentation, null, Boolean.FALSE);
 
-    this.repository.save(event);
+    this.repository.save(this.event);
 
-    Optional<Event> result = this.repository.findById(id);
+    Optional<Event> result = this.repository.findById(this.event.getId());
     assertTrue(result.isPresent());
 
     EventPresentation nextPresentation
         = new EventPresentation("Event New Name", "This is next event.");
-    event.editPresentation(nextPresentation);
+    this.event.editPresentation(nextPresentation);
     Location nextLocation = new Location("Tokyo Example Club", "1-4 Super city, Tokyo");
-    event.changeLocation(nextLocation);
+    this.event.changeLocation(nextLocation);
 
-    this.repository.save(event);
-    Optional<Event> nextResult = this.repository.findById(id);
+    this.repository.save(this.event);
+    Optional<Event> nextResult = this.repository.findById(this.event.getId());
+    
     assertEquals(event.getId(), nextResult.get().getId());
     assertEquals(event.getPresentation(), nextResult.get().getPresentation());
     assertEquals(event.getLocation(), nextResult.get().getLocation());
